c5486dc957928a1527bf621fa232cab362ecbd71
Felix Moser <Fmoser84@gmail.com>
2026-01-28 04:40:48 +0100

    add: more UI fixes

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/felixmoser/Github/StellaWorkspace/STELLA_Documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/felixmoser/Github/StellaWorkspace/STELLA_Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/felixmoser/Github/StellaWorkspace/STELLA_Documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/felixmoser/Github/StellaWorkspace/STELLA_Documentation/src/css/custom.css"),
+  require("/Users/felixmoser/Github/StellaWorkspace/STELLA_Documentation/src/clientModules/scrollDetector.js"),
+];
